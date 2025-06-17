075c169fb2c4c65ac53e0987c565188d9a3835f5
Yashnogida <ryankendall.ryan@gmail.com>
2025-06-17 03:21:37 -0700

    making gains on _rudiments rulefile
    TODO: Remove entries from the "Other Singles/Doubles" subsection in the
    _rudiments rulefile if they already exist in the paradiddles or doubles
    subsection.

--- rules/_rudiments.py
+++ rules/_rudiments.py
@@ -18,6 +18,10 @@ def create_rulefile(title):
     paradiddles = rotate_notes([0, 1, 0, 0, 1, 0, 1, 1])  # RLRR LRLL
     paradiddles = format_notes(paradiddles)
     
+    other_singles_doubles = generate_notes()
+    other_singles_doubles = format_notes(other_singles_doubles)
+
+    
     with open("ly/staff.ly", "w") as file:
       
       rulefile_write_title(file, title)
@@ -27,6 +31,10 @@ def create_rulefile(title):
       
       rulefile_write_section_title(file, "Paradiddles")
       rulefile_write_rhythymic_staff(file, "2/4", paradiddles)
+      
+      rulefile_write_section_title(file, "Other Singles/Doubles")
+      rulefile_write_rhythymic_staff(file, "2/4", other_singles_doubles)
+
   
 def rotate_notes(note_pattern):
     
@@ -38,6 +46,44 @@ def rotate_notes(note_pattern):
     note_data.append(rotated_notes)
 
   return note_data
+
+
+def generate_notes():
+  
+  note_array = []
+  note_data = []
+  
+  for i in range (pow(len(note_sym), notes_per_measure)):
+  
+    note_array = convert_base(i, len(note_sym))
+
+    while(len(note_array) < notes_per_measure):
+      note_array.insert(0, 0)
+
+    note_array = [note_sym[x] for x in note_array]
+    
+    if (apply_rule(note_array)):
+      note_data.append(note_array)
+  
+  return note_data
+
+
+def apply_rule(note_array):   
+  
+  for note in range(len(note_array)):  
+
+    na_n2 = note_array[-2 + note]
+    na_n1 = note_array[-1 + note]
+    na_0 = note_array[note]
+    
+    # No more than two left-hands next to eachother
+    if (("L" in na_n2) and ("L" in na_n1) and ("L" in na_0)):
+        return False
+    
+    if (("R" in na_n2) and ("R" in na_n1) and ("R" in na_0)):
+        return False
+
+  return True
 
 
 def format_notes(note_data):
